6e85335fbb189f7cc9b707c02392330bc6e738ab
jonghyeon <whdgus003@gmail.com>
2022-08-09 21:55:01 +0900

    leetcode 823

--- src/programmers/dfs/단어변환/Solution.java
+++ src/programmers/dfs/단어변환/Solution.java
@@ -18,36 +18,21 @@ public class Solution {
             while(!temp.isEmpty()){
                 wordList.remove(temp.poll());
             }
-//            wordList.removeAll(q.stream().toList());
+            wordList.removeAll(q.stream().toList());
             String curr = q.poll();
-            if (curr == begin) {
-                break;
-            }
             for (String word : wordList) {
                 int cnt = 0;
                 for (int i = 0; i < curr.length(); i++) {
                     if (curr.charAt(i) == word.charAt(i)) cnt++;
                 }
-
                 if (cnt == curr.length() - 1) {
                     q.add(word);
                     resultMap.put(word, resultMap.getOrDefault(curr, 0) + 1);
                 }
             }
-
         }
-        if (!resultMap.keySet().contains(begin)) {
-            return 0;
-        }
-
+        if (!resultMap.keySet().contains(begin)) return 0;
         return resultMap.get(begin);
     }
 
-    public static void main(String[] args) {
-        String begin = "hit";
-        String target = "cog";
-        String[] words = new String[]{"hot", "dot", "dog", "lot", "log", "cog"};
-
-        System.out.println(solution(begin, target, words));
-    }
 }

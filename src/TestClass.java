b2dd5c0a3db91e9b88724782750428f268ab89df
jonghyeon <whdgus003@gmail.com>
2022-08-20 22:48:25 +0900

    testdome

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 class TestClass {
@@ -134,6 +136,129 @@ class Roots {
     public Roots(double x1, double x2) {
         this.x1 = x1;
         this.x2 = x2;
+    }
+}
+
+class UserInput {
+
+    public static class TextInput {
+
+        String value = "";
+
+        public void add(char c){
+            value += c;
+        }
+
+        public String getValue(){
+            return this.value;
+        }
+    }
+
+    public static class NumericInput extends TextInput{
+        @Override
+        public void add(char c) {
+            if (c >= '0' && c <= '9')  super.add(c);
+        }
+    }
+
+    public static void main(String[] args) {
+        TextInput input = new NumericInput();
+        input.add('1');
+        input.add('a');
+        input.add('0');
+        System.out.println(input.getValue());
+    }
+}
+
+//---------------------------------------------------------------------
+
+class DecoratorStream extends OutputStream
+{
+    private OutputStream stream;
+    private String prefix;
+
+
+    DecoratorStream(OutputStream stream, String prefix) {
+        super();
+        this.stream = stream;
+        this.prefix = prefix;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        byte[] result = new byte[4];
+
+        result[0] = (byte) (b >> 24);
+        result[1] = (byte) (b >> 16);
+        result[2] = (byte) (b >> 8);
+        result[3] = (byte) (b);
+
+        write(result, 0, 4);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        if (prefix != null) {
+            stream.write(prefix.getBytes(StandardCharsets.UTF_8));
+            prefix = null;
+        }
+        stream.write(b, off, len);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        write(b, 0, b.length);
+    }
+
+    public static void main(String[] args) throws IOException {
+        byte[] message = new byte[]{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21};
+        try(ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            DecoratorStream decoratorStream = new DecoratorStream(baos, "First line: ");
+            decoratorStream.write(message);
+
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), "UTF-8"))) {
+                System.out.println(reader.readLine());  //should print "First line: Hello, world!"
+            }
+        }
+    }
+}
+
+class Folders {
+    public static Collection<String> folderNames(String xml, char startingLetter) throws Exception {
+        List<String> names = new ArrayList<>();
+        while (xml.contains("folder name=")){
+
+            xml = xml.substring(xml.indexOf("folder name=\"") + 13);
+            String name = xml.substring(0, xml.indexOf("\""));
+            if (name.charAt(0) == startingLetter) names.add(name);
+            xml = xml.substring(xml.indexOf("\"")+1);
+
+        }
+
+        return names;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String xml =
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                        "<folder name=\"c\">" +
+                        "<folder name=\"program files\">" +
+                        "<folder name=\"uninstall information\" />" +
+                        "</folder>" +
+                        "<folder name=\"users\" />" +
+                        "</folder>";
+
+        Collection<String> names = folderNames(xml, 'u');
+        for(String name: names)
+            System.out.println(name);
+
+//        String a = "folder name=\"aasdkjh\"";
+//
+//        System.out.println(a.indexOf("folder name=\""));
+//
+//        a = a.substring(a.indexOf("folder name=\"") + 13);
+//        a = a.substring(a.indexOf("\""));
+//        System.out.println(a);
     }
 }
 

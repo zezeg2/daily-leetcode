f9503e97b6a1b775214c5ac2b2dfc288e09e5351
jonghyeon <whdgus003@gmail.com>
2022-08-22 19:04:13 +0900

    testdome

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,6 +1,13 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
+import java.util.stream.Collectors;
 
 class TestClass {
 }
@@ -99,7 +106,7 @@ class IceCreamMachine {
 
 class MergeNames {
 
-        public static String[] uniqueNames(String[] names1, String[] names2) {
+    public static String[] uniqueNames(String[] names1, String[] names2) {
         Set<String> set = new TreeSet<>();
         set.addAll(Arrays.asList(names1));
         set.addAll(Arrays.asList(names2));
@@ -121,7 +128,7 @@ class QuadraticEquation {
         double discriminant = Math.pow(b, 2) - 4 * (a * c);
         x1 = ((-b) + Math.sqrt(discriminant)) / (2 * a);
         x2 = ((-b) - Math.sqrt(discriminant)) / (2 * a);
-        return new Roots(x1,x2);
+        return new Roots(x1, x2);
     }
 
     public static void main(String[] args) {
@@ -145,19 +152,19 @@ class UserInput {
 
         String value = "";
 
-        public void add(char c){
+        public void add(char c) {
             value += c;
         }
 
-        public String getValue(){
+        public String getValue() {
             return this.value;
         }
     }
 
-    public static class NumericInput extends TextInput{
+    public static class NumericInput extends TextInput {
         @Override
         public void add(char c) {
-            if (c >= '0' && c <= '9')  super.add(c);
+            if (c >= '0' && c <= '9') super.add(c);
         }
     }
 
@@ -170,10 +177,29 @@ class UserInput {
     }
 }
 
+
+class TwoSum {
+    public static int[] findTwoSum(int[] list, int sum) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < list.length; i++) {
+            int findElement = sum - list[i];
+            if (map.get(findElement) != null) return new int[]{i, map.get(findElement)};
+            map.put(list[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] indices = findTwoSum(new int[]{3, 1, 5, 7, 5, 9}, 10);
+        if (indices != null) {
+            System.out.println(indices[0] + " " + indices[1]);
+        }
+    }
+}
+
 //---------------------------------------------------------------------
 
-class DecoratorStream extends OutputStream
-{
+class DecoratorStream extends OutputStream {
     private OutputStream stream;
     private String prefix;
 
@@ -212,11 +238,11 @@ class DecoratorStream extends OutputStream
 
     public static void main(String[] args) throws IOException {
         byte[] message = new byte[]{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21};
-        try(ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             DecoratorStream decoratorStream = new DecoratorStream(baos, "First line: ");
             decoratorStream.write(message);
 
-            try(BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), "UTF-8"))) {
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), "UTF-8"))) {
                 System.out.println(reader.readLine());  //should print "First line: Hello, world!"
             }
         }
@@ -224,19 +250,21 @@ class DecoratorStream extends OutputStream
 }
 
 class Folders {
+
     public static Collection<String> folderNames(String xml, char startingLetter) throws Exception {
-        List<String> names = new ArrayList<>();
-        while (xml.contains("folder name=")){
-
-            xml = xml.substring(xml.indexOf("folder name=\"") + 13);
-            String name = xml.substring(0, xml.indexOf("\""));
-            if (name.charAt(0) == startingLetter) names.add(name);
-            xml = xml.substring(xml.indexOf("\"")+1);
-
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        Document doc = builder.parse(is);
+        NodeList node = doc.getElementsByTagName("folder");
+        Collection<String> result = new ArrayList<>();
+        for (int i = 0; i < node.getLength(); i++) {
+            String nodeName = node.item(i).getAttributes().getNamedItem("name").getNodeValue();
+            if (nodeName.toCharArray()[0] == startingLetter) {
+                result.add(nodeName);
+            }
         }
-
-
-        return names;
+        return result;
     }
 
     public static void main(String[] args) throws Exception {
@@ -250,7 +278,7 @@ class Folders {
                         "</folder>";
 
         Collection<String> names = folderNames(xml, 'u');
-        for(String name: names)
+        for (String name : names)
             System.out.println(name);
 
 //        String a = "folder name=\"aasdkjh\"";
@@ -262,4 +290,157 @@ class Folders {
 //        System.out.println(a);
     }
 }
+
+class Node {
+    public int value;
+    public Node left, right;
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class BinarySearchTree {
+    public static boolean contains(Node root, int value) {
+        if (root.value == value) return true;
+        else if (root.value > value) {
+            if (root.left == null) return false;
+            return contains(root.left, value);
+        } else {
+            if (root.right == null) return false;
+            return contains(root.right, value);
+        }
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(1, null, null);
+        Node n3 = new Node(3, null, null);
+        Node n2 = new Node(2, n1, n3);
+
+        System.out.println(contains(n2, 3));
+    }
+}
+
+class Song {
+    private String name;
+    private Song nextSong;
+
+    public Song(String name) {
+        this.name = name;
+    }
+
+    public Song getNextSong() {
+        return nextSong;
+    }
+
+    public void setNextSong(Song nextSong) {
+        this.nextSong = nextSong;
+    }
+
+    public boolean isRepeatingPlaylist() {
+        Set<Song> list = new HashSet<>();
+        Song current = this;
+        while (current.getNextSong() != null) {
+            if (list.contains(current.getNextSong())) {
+                return true;
+            }
+            list.add(current);
+            current = current.getNextSong();
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Song first = new Song("Hello");
+        Song second = new Song("Eye of the tiger");
+
+        first.setNextSong(second);
+        second.setNextSong(first);
+
+        System.out.println(first.isRepeatingPlaylist());
+    }
+}
+
+class SortedSearch {
+
+    public static int countNumbers1(int[] sortedArray, int lessThan) {
+        int start = 0;
+        int end = sortedArray.length - 1;
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (sortedArray[mid] < lessThan) {
+                if (mid < sortedArray.length - 1 && sortedArray[mid + 1] < lessThan) { // check id next value is also valid
+                    start = mid + 1;
+                    continue;
+                } else
+                    return mid + 1;
+            }
+
+            if (sortedArray[mid] >= lessThan) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return 0;
+
+    }
+    public static int countNumbers(int[] sortedArray, int lessThan) {
+        int start = 0;
+        int end = sortedArray.length - 1;
+        int mid = 0;
+
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (sortedArray[mid] < lessThan) {
+                if (mid < sortedArray.length - 1 && sortedArray[mid + 1] < lessThan) { // check id next value is also valid
+                    start = mid + 1;
+                    continue;
+                } else
+                    return mid + 1;
+            }
+            if (sortedArray[mid] >= lessThan) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SortedSearch.countNumbers(new int[]{0,1,2,3,5,7,9,11,14}, 4));
+    }
+}
+
+
+class TrainComposition {
+
+    LinkedList<Integer> wagons = new LinkedList<Integer>();
+    public void attachWagonFromLeft(int wagonId) {
+        wagons.addFirst(wagonId);
+    }
+    public void attachWagonFromRight(int wagonId) {
+        wagons.addLast(wagonId);
+    }
+    public int detachWagonFromLeft() {
+        if (!wagons.isEmpty()) return wagons.removeFirst();
+        else throw new IndexOutOfBoundsException("No wagons available");
+    }
+    public int detachWagonFromRight() {
+        if (!wagons.isEmpty()) return wagons.removeLast();
+        else throw new IndexOutOfBoundsException("No wagons available");
+    }
+    public static void main (String[] args){
+        TrainComposition train = new TrainComposition();
+        train.attachWagonFromLeft(7);
+        train.attachWagonFromLeft(13);
+        System.out.println(train.detachWagonFromRight()); // 7
+        System.out.println(train.detachWagonFromLeft()); // 13
+    }
+}
+
 

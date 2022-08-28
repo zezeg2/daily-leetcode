702c23f29fa5c773c97b795e3396ae463d29b6f9
jonghyeon <whdgus003@gmail.com>
2022-08-28 17:57:25 +0900

    프로그래머스 행렬과 연산, 두큐합같게만들기

--- src/programmers/etc/두큐합같게만들기/Solution.java
+++ src/programmers/etc/두큐합같게만들기/Solution.java
@@ -3,27 +3,24 @@ package programmers.etc.두큐합같게만들기;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Solution{
+public class Solution {
     public int solution(int[] queue1, int[] queue2) {
         Queue<Integer> que1 = new LinkedList<>();
         Queue<Integer> que2 = new LinkedList<>();
 
         long sum1 = 0, sum2 = 0;
-        for(int i = 0; i < queue1.length; i++) {
+        for (int i = 0; i < queue1.length; i++) {
             sum1 += queue1[i];
             que1.offer(queue1[i]);
             sum2 += queue2[i];
             que2.offer(queue2[i]);
         }
 
-        for(int i = 0; i < queue2.length; i++) {
-
-        }
         int count = 0;
-        while(sum1 != sum2) {
+        while (sum1 != sum2) {
             count++;
 
-            if(sum1 > sum2) {
+            if (sum1 > sum2) {
                 int value = que1.poll();
                 sum1 -= value;
                 sum2 += value;
@@ -35,35 +32,34 @@ public class Solution{
                 que1.offer(value);
             }
 
-            if(count > (queue1.length + queue2.length) * 2) return -1;
+            if (count > (queue1.length + queue2.length) * 2) return -1;
         }
 
         return count;
     }
 }
+
 class WrongSolution {
 
     public static int solution(int[] queue1, int[] queue2) {
         int length = queue1.length;
         long s1 = 0;
         long s2 = 0;
-        Queue<Long> q1 = new LinkedList<>();
-        Queue<Long> q2 = new LinkedList<>();
-        for(int i =0; i < length ; i++) {
-            s1 += queue1[i] ;
-            s2 += queue2[i] ;
-            q1.add((long) queue1[i]) ;
-            q2.add((long) queue2[i]) ;
-
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+        for (int i = 0; i < length; i++) {
+            s1 += queue1[i];
+            s2 += queue2[i];
+            q1.offer(queue1[i]);
+            q2.offer(queue2[i]);
         }
-        if ((s1 + s2) % 2 == 1) return -1;
         long target = (s1 + s2) / 2;
         int count = 0;
 
-        while (q1.size() != 2 * queue1.length && q2.size() != 2 * queue2.length){
-            long polled;
-            if (s1 < target){
-                 polled =  q2.poll();
+        while (s1 != s2) {
+            int polled;
+            if (s1 < s2) {
+                polled = q2.poll();
                 s1 += polled;
                 s2 -= polled;
                 q1.add(polled);
@@ -74,14 +70,14 @@ class WrongSolution {
                 q2.add(polled);
             }
             count++;
-            if (s1 == target || s2 == target) return count;
+            if (count > (queue1.length + queue2.length) * 2) return -1;
         }
-        return -1;
+        return count;
     }
 
     public static void main(String[] args) {
-        int[] queue1 = {1,1};
-        int[] queue2 = {1,5};
-        System.out.println(solution(queue1,queue2));
+        int[] queue1 = {1, 1};
+        int[] queue2 = {1, 5};
+        System.out.println(solution(queue1, queue2));
     }
 }

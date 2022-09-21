4967dd7dab55d2dcd906b92851e46beb1cfc1269
jonghyeon <whdgus003@gmail.com>
2022-09-21 16:02:03 +0900

    프로그래머스 부족한 금액 계산

---
+++ src/programmers/lv1/부족한금액계산/Solution.java
@@ -0,0 +1,9 @@
+package programmers.lv1.부족한금액계산;
+
+public class Solution {
+    public long solution(int price, int money, int count) {
+        long longMoney = Long.valueOf(money);
+        longMoney -= (long) price * (count * (count + 1)) / 2;
+        return longMoney >= 0 ? 0 : -longMoney;
+    }
+}

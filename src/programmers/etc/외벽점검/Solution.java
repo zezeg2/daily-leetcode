5695be910737f8cab2669487bfb5fa3b81eadcc6
jonghyeon <whdgus003@gmail.com>
2022-10-06 13:08:35 +0900

    키패드누르기

--- src/programmers/etc/외벽점검/Solution.java
+++ src/programmers/etc/외벽점검/Solution.java
@@ -6,13 +6,14 @@ import java.util.stream.Collectors;
 public class Solution {
     static int N;
     static List<Integer> weakList;
+
     public static int solution(int n, int[] weak, int[] dist) {
         N = n;
         weakList = Arrays.stream(weak).boxed().collect(Collectors.toList());
         Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         for (int e : dist) pq.add(e);
 
-        while (!pq.isEmpty()){
+        while (!pq.isEmpty()) {
             if (weakList.isEmpty()) break;
             int maxWeakNum = -1;
             int maxIndex = -1;
@@ -20,12 +21,11 @@ public class Solution {
             for (int i = 0; i < weakList.size(); i++) {
                 int prevMax = maxWeakNum;
                 maxWeakNum = Math.max(maxWeakNum, getWeakNum(poll, weakList.get(i)));
-                if (prevMax!=maxWeakNum) maxIndex = i;
+                if (prevMax != maxWeakNum) maxIndex = i;
             }
             inspectWeak(poll, weakList.get(maxIndex));
         }
         if (!weakList.isEmpty()) return -1;
-
 
 
         return dist.length - pq.size();
@@ -40,14 +40,14 @@ public class Solution {
     private static void inspectWeak(int d, int loc) {
         int maxRange = d + loc;
         if (maxRange > N) {
-            for (int i = 0; i < weakList.size(); i++){
+            for (int i = 0; i < weakList.size(); i++) {
                 if ((weakList.get(i) >= loc && weakList.get(i) < N) || N <= maxRange - N) {
                     weakList.remove(i);
                     i--;
                 }
             }
         } else {
-            for (int i = 0; i < weakList.size(); i++){
+            for (int i = 0; i < weakList.size(); i++) {
                 if ((weakList.get(i) >= loc && weakList.get(i) < maxRange)) {
                     weakList.remove(i);
                     i--;
@@ -57,44 +57,44 @@ public class Solution {
     }
 
     private static int ret;
+
     public static int solution2(int n, int[] weak, int[] dist) {
-        ret = n+1;
-        int[] w = new int[weak.length*2];
+        ret = n + 1;
+        int[] expanded = new int[weak.length * 2];
         int idx = 0;
-        // 역방향 계산하기 짜증나서 만들어 버림.
-        while(idx<w.length) {
-            if(idx<weak.length) w[idx] = weak[idx];
-            else w[idx] = n+weak[idx-weak.length];
+
+        while (idx < expanded.length) {
+            if (idx < weak.length) expanded[idx] = weak[idx];
+            else expanded[idx] = n + weak[idx - weak.length];
             idx++;
         }
-        for(int i=0 ; i<weak.length ; i++) {
-            dfs(w, dist, new boolean[dist.length], n, i, weak.length+i, 0);
+        for (int i = 0; i < weak.length; i++) {
+            dfs(expanded, dist, new boolean[dist.length], i, weak.length + i, 0);
         }
-        return ret>n?-1:ret;
+        return ret > n ? -1 : ret;
     }
 
-    public static void dfs(int[] weak, int[] dist, boolean[] visit, int n, int start, int end, int sum) {
-        if(start>=end) {
+    public static void dfs(int[] weak, int[] dist, boolean[] visit, int start, int end, int sum) {
+        if (start >= end) {
             ret = Math.min(ret, sum);
             return;
         }
-        for(int i=0; i<dist.length ; i++) {
-            if(visit[i]) continue;
+        for (int i = 0; i < dist.length; i++) {
+            if (visit[i]) continue;
             visit[i] = true;
-            int idx = start+1;
-            while(idx<end && (weak[idx]-weak[start])<=dist[i] ) {
+            int idx = start + 1;
+            while (idx < end && (weak[idx] - weak[start]) <= dist[i]) {
                 idx++;
             }
-            dfs(weak, dist, visit, n, idx, end, sum+1);
+            dfs(weak, dist, visit, idx, end, sum + 1);
             visit[i] = false;
         }
         return;
     }
 
 
-
     public static void main(String[] args) {
-        System.out.println(solution2(12, new int[]{1, 5, 6, 10},new int[]{1, 2, 3, 4}));
+        System.out.println(solution2(12, new int[]{1, 5, 6, 10}, new int[]{1, 2, 3, 4}));
     }
 
 }
---
+++ src/programmers/키패드누르기/Solution.java
@@ -0,0 +1,53 @@
+package programmers.키패드누르기;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static String solution(int[] numbers, String hand) {
+        String answer = "";
+        Map<Integer, Location> map = new HashMap<>();
+        map.put(0, new Location(3,1));
+        for (int i = 1; i < 10; i++) map.put(i, new Location((i - 1) / 3, (i-1) % 3));
+        Location left = new Location(3,0);
+        Location right = new Location(3,2);
+
+        for (int n : numbers) {
+            String s = "";
+            Location sel = map.get(n);
+            if (n == 1 || n == 4 || n == 7) s = "L";
+            else if (n == 3 || n == 6 || n == 9) s = "R";
+            else {
+                int ld = getDistance(left, sel);
+                int rd = getDistance(right, sel);
+                if (ld < rd) s = "L";
+                else if (ld > rd) s = "R";
+                else {
+                    if (hand == "left") s = "L";
+                    if (hand == "right") s = "R";
+                }
+            }
+            if (s.equals("L")) left = sel;
+            if (s.equals("R")) right = sel;
+            answer += s;
+        }
+        return answer;
+    }
+
+    static class Location {
+        int r, c;
+
+        public Location(int r, int c) {
+            this.r = r;
+            this.c = c;
+        }
+    }
+
+    static int getDistance(Location l1, Location l2){
+        return Math.abs(l1.r - l2.r) + Math.abs(l1.c - l2.c);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2}, "left"));
+    }
+}

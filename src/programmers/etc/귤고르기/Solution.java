075f4933015bcaabb0679dfe77e2f306a180662b
jonghyeon <whdgus003@gmail.com>
2022-12-11 20:46:24 +0900

    프로그래머스 귤고르기

---
+++ src/programmers/etc/귤고르기/Solution.java
@@ -0,0 +1,37 @@
+package programmers.etc.귤고르기;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int solution(int limit, int[] tangerine) {
+        int answer = 0;
+
+        int count = 0;
+
+        Map<Integer, Integer> counter = new HashMap<>();
+
+        for (int tangerineNumber : tangerine) {
+            int curTangerine = counter.getOrDefault(tangerineNumber, 0);
+            curTangerine++;
+            counter.put(tangerineNumber, curTangerine);
+        }
+        List<Integer> tangerines = new ArrayList<>(counter.keySet());
+
+        tangerines.sort((a, b) -> counter.get(b) - counter.get(a));
+
+        for (Integer keyIdx : tangerines) {
+            count += counter.get(keyIdx);
+            answer++;
+
+            if (count >= limit) {
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
+

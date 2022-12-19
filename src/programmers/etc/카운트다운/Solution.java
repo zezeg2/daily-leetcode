ed202b78939a4c695f5ec57b049b8344d86f7b6a
zezeg2 <whdgus003@gmail.com>
2022-12-19 20:51:21 +0900

    카운트다운 풀이중

---
+++ src/programmers/etc/카운트다운/Solution.java
@@ -0,0 +1,20 @@
+package programmers.etc.카운트다운;
+
+public class Solution {
+//    public int[] solution(int target) {
+//        int leftScore = target % 60;
+//        int count = target / 60;
+//        if (leftScore == 0) return new int[]{count, 0};
+//        if (count == 0) {
+//            if (leftScore <= 20) return new int[]{count + 1, 1};
+//            for (int i = 1; i < 20; i++) {
+//                if (leftScore == 2 * i || leftScore == 3 * i) return new int[]{count + 1, 0};
+//            }
+//            if (leftScore > 57) return new int[]{count + 2, 2};
+//        } else {
+//            if (leftScore <= 10) return new int[]{count + 1, 2};
+//            for (int i = 1; i < 20; i++) if (leftScore == 2 * i || leftScore == 3 * i) return new int[]{count + 1, 0};
+//            if (leftScore > 47) return new int[]{count + 2, 2};
+//        }
+//    }
+}

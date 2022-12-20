ec65001dd778114c71f4b17e13af9a361d437e43
zezeg2 <whdgus003@gmail.com>
2022-12-20 10:09:17 +0900

    프로그래머스 카운트다운

--- src/programmers/etc/카운트다운/Solution.java
+++ src/programmers/etc/카운트다운/Solution.java
@@ -1,20 +1,29 @@
 package programmers.etc.카운트다운;
 
-public class Solution {
-//    public int[] solution(int target) {
-//        int leftScore = target % 60;
-//        int count = target / 60;
-//        if (leftScore == 0) return new int[]{count, 0};
-//        if (count == 0) {
-//            if (leftScore <= 20) return new int[]{count + 1, 1};
-//            for (int i = 1; i < 20; i++) {
-//                if (leftScore == 2 * i || leftScore == 3 * i) return new int[]{count + 1, 0};
-//            }
-//            if (leftScore > 57) return new int[]{count + 2, 2};
-//        } else {
-//            if (leftScore <= 10) return new int[]{count + 1, 2};
-//            for (int i = 1; i < 20; i++) if (leftScore == 2 * i || leftScore == 3 * i) return new int[]{count + 1, 0};
-//            if (leftScore > 47) return new int[]{count + 2, 2};
-//        }
-//    }
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+class Solution {
+    static int[][] table;
+    public static int[] solution(int target) {
+        table = new int[target + 1][2];
+        IntStream.range(1, target + 1).forEach(i -> {
+            if (i <= 20 || i == 50) table[i] = new int[]{1, 1};
+            else if (i <= 40 && i % 2 == 0) table[i] = new int[]{1, 0};
+            else if (i <= 60 && i % 3 == 0) table[i] = new int[]{1, 0};
+            else if (i > 40 && i <= 50) table[i] = new int[]{2, 1};
+            else if (i <= 70) table[i] = new int[]{2, 2};
+            else {
+                table[i] = table[i - 60][0] == table[i - 50][0] ? new int[]{table[i - 50][0] + 1, Math.max(table[i - 60][1], table[i - 50][1] + 1)}
+                        : table[i - 60][0] < table[i - 50][0] ? new int[]{table[i - 60][0] + 1, table[i - 60][1]}
+                        : new int[]{table[i - 50][0] + 1, table[i - 50][1] + 1};
+            }
+        });
+        return table[target];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(101)));
+
+    }
 }
